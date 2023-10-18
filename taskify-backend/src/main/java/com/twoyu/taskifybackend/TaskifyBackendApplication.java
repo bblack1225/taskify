94950269ce70a5488f4e4a6b0509edb3210b1a5c
bblack0137 <black60137@gmail.com>
2023-10-18 22:32:57 +0800

    feat: project init

---
+++ taskify-backend/src/main/java/com/twoyu/taskifybackend/TaskifyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.twoyu.taskifybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskifyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskifyBackendApplication.class, args);
+    }
+
+}
---
+++ taskify-backend/src/test/java/com/twoyu/taskifybackend/TaskifyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twoyu.taskifybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskifyBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
