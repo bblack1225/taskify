9e0caabe09dfb59c90cd2a31b288065daaaf968f
bblack0137 <black60137@gmail.com>
2023-10-22 15:35:19 +0800

    feat: 新增column api

--- taskify-backend/src/main/java/com/twoyu/taskifybackend/controller/StatusColumnController.java
+++ taskify-backend/src/main/java/com/twoyu/taskifybackend/controller/StatusColumnController.java
@@ -1,9 +1,24 @@
 package com.twoyu.taskifybackend.controller;
 
+import com.twoyu.taskifybackend.model.vo.request.AddColumnRequest;
+import com.twoyu.taskifybackend.model.vo.response.AddColumnRes;
+import com.twoyu.taskifybackend.service.IStatusColumnService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/status-column")
+@RequestMapping("/statusCol")
+@RequiredArgsConstructor
 public class StatusColumnController {
+    private final IStatusColumnService statusColumnService;
+
+    @PostMapping
+    public AddColumnRes addColumn(@Valid @RequestBody AddColumnRequest request){
+        return statusColumnService.addColumn(request);
+    }
+
 }
--- taskify-backend/src/main/java/com/twoyu/taskifybackend/controller/WorkSpaceController.java
+++
@@ -1,9 +0,0 @@
-package com.twoyu.taskifybackend.controller;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/workspace")
-public class WorkSpaceController {
-}
