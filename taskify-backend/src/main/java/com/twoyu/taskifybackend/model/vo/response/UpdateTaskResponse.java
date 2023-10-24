1dd8174ec4d10e4366ef45aa1266a4afe0adf65b
bblack1225 <black60137@gmail.com>
2023-10-24 12:05:04 +0800

    feat: addColumn 成功版 unit test

--- taskify-backend/src/main/java/com/twoyu/taskifybackend/model/vo/response/UpdateTaskResponse.java
+++ taskify-backend/src/main/java/com/twoyu/taskifybackend/model/vo/response/UpdateTaskResponse.java
@@ -3,5 +3,5 @@ package com.twoyu.taskifybackend.model.vo.response;
 import java.util.List;
 import java.util.UUID;
 
-public record UpdateTaskResponse(UUID id, String name, String description, int dataIndex, UUID statusColumnId, List<UUID> labels) {
+public record  UpdateTaskResponse(UUID id, String name, String description, int dataIndex, UUID statusColumnId, List<UUID> labels) {
 }
---
+++ taskify-backend/src/test/java/com/twoyu/taskifybackend/service/impl/StatusColumnServiceTest.java
@@ -0,0 +1,57 @@
+package com.twoyu.taskifybackend.service.impl;
+
+import com.twoyu.taskifybackend.model.entity.StatusColumn;
+import com.twoyu.taskifybackend.model.vo.request.AddColumnRequest;
+import com.twoyu.taskifybackend.model.vo.response.AddColumnResponse;
+import com.twoyu.taskifybackend.repository.StatusColumnRepository;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class StatusColumnServiceTest {
+
+    @MockBean
+    private StatusColumnRepository statusColumnRepository;
+
+    @Autowired
+    private StatusColumnService statusColumnService;
+
+
+        // Given a valid AddColumnRequest, it should create a new StatusColumn with the same properties as the request and return an AddColumnResponse with the same properties as the created StatusColumn.
+        @Test
+        public void test_validAddColumnRequest() {
+            // Create a valid AddColumnRequest
+            AddColumnRequest addColumnRequest = new AddColumnRequest();
+            addColumnRequest.setBoardId(UUID.randomUUID());
+            addColumnRequest.setTitle("Test Title");
+            addColumnRequest.setDataIndex(0);
+
+            // Mock the statusColumnRepository.save() method
+            StatusColumn savedStatusColumn = new StatusColumn();
+            savedStatusColumn.setId(UUID.randomUUID());
+            savedStatusColumn.setBoardId(addColumnRequest.getBoardId());
+            savedStatusColumn.setTitle(addColumnRequest.getTitle());
+            savedStatusColumn.setDataIndex(addColumnRequest.getDataIndex());
+            Mockito.when(statusColumnRepository.save(Mockito.any(StatusColumn.class))).thenReturn(savedStatusColumn);
+
+            // Call the addColumn method
+            AddColumnResponse response = statusColumnService.addColumn(addColumnRequest);
+
+            // Verify that the statusColumnRepository.save() method was called with the correct arguments
+            Mockito.verify(statusColumnRepository).save(Mockito.any(StatusColumn.class));
+
+            // Verify that the response has the same properties as the created StatusColumn
+            assertEquals(savedStatusColumn.getId(), response.id());
+            assertEquals(savedStatusColumn.getBoardId(), response.boardId());
+            assertEquals(savedStatusColumn.getTitle(), response.title());
+            assertEquals(savedStatusColumn.getDataIndex(), response.dataIndex());
+        }
+
+}
