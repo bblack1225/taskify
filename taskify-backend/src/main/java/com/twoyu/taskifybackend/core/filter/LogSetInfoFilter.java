1b77a4b375c77c0628216d8df98663eb22aba45e
bblack1225 <black60137@gmail.com>
2023-10-30 09:18:27 +0800

    feat: response adapter

--- taskify-backend/src/main/java/com/twoyu/taskifybackend/core/filter/LogSetInfoFilter.java
+++ taskify-backend/src/main/java/com/twoyu/taskifybackend/core/filter/LogSetInfoFilter.java
@@ -2,11 +2,9 @@ package com.twoyu.taskifybackend.core.filter;
 
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.MDC;
 import org.springframework.stereotype.Component;
-import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.util.UUID;
@@ -14,13 +12,6 @@ import java.util.UUID;
 @Component
 @Slf4j
 public class LogSetInfoFilter implements Filter {
-//    @Override
-//    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-//        String singleProcessId = UUID.randomUUID().toString();
-//        MDC.put("singleProcessId", singleProcessId);
-//        MDC.put("reqURI", request.getRequestURI());
-//        filterChain.doFilter(request, response);
-//    }
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
@@ -29,7 +20,6 @@ public class LogSetInfoFilter implements Filter {
 
         MDC.put("singleProcessId", singleProcessId);
         MDC.put("reqURI", request.getRequestURI());
-        log.info("reqURI: {}", request.getRequestURI());
         filterChain.doFilter(servletRequest, servletResponse);
     }
 }
