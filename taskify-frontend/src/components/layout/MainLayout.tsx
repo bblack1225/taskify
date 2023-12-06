876ea625480b91de5a1a8fbfa77fd96803dea34f
Keely <hellennnnn1@gmail.com>
2023-12-06 10:44:10 +0800

    feat: 新增控制navBoard開啟或關閉的state及css，另外還加入可愛的caret-color

--- taskify-frontend/src/components/layout/MainLayout.tsx
+++ taskify-frontend/src/components/layout/MainLayout.tsx
@@ -1,19 +1,28 @@
 import { AppShell } from "@mantine/core";
 import style from "./MainLayout.module.scss";
 import NavBoard from "./Navboard";
+import { useState } from "react";
 
 type Props = {
   children: React.ReactNode;
 };
 
 function MainLayout({ children }: Props) {
+  const [isNavBoardOpen, setIsNavBoardOpen] = useState(true);
   return (
     <AppShell header={{ height: 60 }} padding="md">
       <AppShell.Header className={style.header}>Taskify</AppShell.Header>
-      <AppShell.Navbar className={style.navbar}>
-        <NavBoard />
+      <AppShell.Navbar
+        className={isNavBoardOpen ? style.navbar : style.navbarClose}
+      >
+        <NavBoard
+          isNavBoardOpen={isNavBoardOpen}
+          setIsNavBoardOpen={setIsNavBoardOpen}
+        />
       </AppShell.Navbar>
-      <AppShell.Main className={style.main}>{children}</AppShell.Main>
+      <AppShell.Main className={isNavBoardOpen ? style.main : style.mainClose}>
+        {children}
+      </AppShell.Main>
     </AppShell>
   );
 }
--- taskify-frontend/src/components/layout/Navboard.tsx
+++ taskify-frontend/src/components/layout/Navboard.tsx
@@ -1,43 +1,88 @@
-import { Box, Button, Stack } from "@mantine/core";
+import { Box, Button, Flex, Stack } from "@mantine/core";
 import Avatar from "/public/lazy.png";
 import {
   IconAlignBoxBottomCenter,
   IconUsers,
   IconCalendarSearch,
   IconHeartDown,
+  IconChevronLeft,
+  IconChevronRight,
 } from "@tabler/icons-react";
 
 import style from "./NavBoard.module.scss";
-import { Link as RouterLink } from "@tanstack/react-router";
+import { Link } from "@tanstack/react-router";
 
-function NavBoard() {
+type Props = {
+  isNavBoardOpen: boolean;
+  setIsNavBoardOpen: (value: boolean) => void;
+};
+
+function NavBoard({ isNavBoardOpen, setIsNavBoardOpen }: Props) {
   return (
-    <Box p={20}>
-      <Box>
-        <div className={style.navTitle}>TwoYu</div>
-        <img src={Avatar} alt="萬聖節快樂" width={"200px"} />
-      </Box>
-      <Stack pt={10}>
-        <Button color="#be3144">
-          <IconAlignBoxBottomCenter />
-          <RouterLink to="/taskify/board">
-            <Box p={5}>看板</Box>
-          </RouterLink>
-        </Button>
-        <Button color="#d55b3e">
-          <IconUsers />
-          <Box p={5}>成員</Box>
-        </Button>
-        <Button color="#be3144">
-          <IconCalendarSearch />
-          <Box p={5}>行事曆</Box>
-        </Button>
-        <Button color="#d55b3e">
-          <IconHeartDown />
-          <Box p={5}>你的看板</Box>
-        </Button>
-      </Stack>
-    </Box>
+    <>
+      {isNavBoardOpen ? (
+        <Box p={20}>
+          <Box>
+            <Flex justify={"space-between"} align={"center"} mb={20}>
+              <Flex className={style.navTitle}>TwoYu</Flex>
+              <IconChevronLeft
+                style={{ cursor: "pointer" }}
+                onClick={() => setIsNavBoardOpen(false)}
+              />
+            </Flex>
+            <img
+              style={{ marginTop: "10px" }}
+              src={Avatar}
+              alt="萬聖節快樂"
+              width={"200px"}
+            />
+          </Box>
+          <Stack pt={10}>
+            <Link
+              to="/board"
+              activeProps={{
+                style: {
+                  borderBottom: "2px solid black",
+                },
+              }}
+            >
+              <Button color="#be3144" w={"200px"}>
+                <IconAlignBoxBottomCenter />
+                <Box p={5}>看板</Box>
+              </Button>
+            </Link>
+            <Button color="#d55b3e">
+              <IconUsers />
+              <Box p={5}>成員</Box>
+            </Button>
+            <Link
+              to="/calendar"
+              activeProps={{
+                style: {
+                  borderBottom: "2px solid black",
+                },
+              }}
+            >
+              <Button color="#be3144" w={"200px"}>
+                <IconCalendarSearch />
+                <Box p={5}>行事曆</Box>
+              </Button>
+            </Link>
+            <Button color="#d55b3e">
+              <IconHeartDown />
+              <Box p={5}>你的看板</Box>
+            </Button>
+          </Stack>
+        </Box>
+      ) : (
+        <Box w={25}>
+          <IconChevronRight
+            style={{ cursor: "pointer", marginTop: "22px" }}
+            onClick={() => setIsNavBoardOpen(true)}
+          />
+        </Box>
+      )}
+    </>
   );
 }
 
