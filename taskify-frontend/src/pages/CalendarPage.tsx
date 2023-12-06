8dea2ce2adf0d2c9edbc147332a5c19140bc422a
bblack1225 <black60137@gmail.com>
2023-12-06 17:47:15 +0800

    calendar 頁面先加上按鈕可以update table的size

--- taskify-frontend/src/pages/CalendarPage.tsx
+++ taskify-frontend/src/pages/CalendarPage.tsx
@@ -1,11 +1,22 @@
-import { Box, Flex, Stack } from "@mantine/core";
+import { Box, Button, Flex, Stack } from "@mantine/core";
 import style from "@/pages/Taskboard.module.scss";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
+import { useRef } from "react";
 
 const events = [{ title: "Meeting", start: new Date() }];
 
 function CalendarPage() {
+  const calendarRef = useRef<InstanceType<typeof FullCalendar> | null>(null);
+
+  const handleClick = () => {
+    const calendarApi = calendarRef?.current?.getApi();
+    console.log("calendarApi", calendarApi);
+
+    if (calendarApi) {
+      calendarApi.updateSize();
+    }
+  };
   return (
     <Stack
       style={{
@@ -15,9 +26,22 @@ function CalendarPage() {
     >
       <Flex className={style.container}>
         <Flex>行事曆</Flex>
+        <Button onClick={handleClick}>click</Button>
       </Flex>
-      <Box style={{ overflow: "hidden auto", paddingRight: "10px" }}>
+      <Box
+        style={{
+          // maxWidth: "100%",
+          height: "100%",
+          overflow: "hidden",
+          paddingRight: "10px",
+        }}
+      >
         <FullCalendar
+          windowResize={() => console.log("resize")}
+          handleWindowResize={true}
+          ref={calendarRef}
+          windowResizeDelay={0}
+          height="100%"
           initialView="dayGridMonth"
           displayEventTime={true}
           events={events}
