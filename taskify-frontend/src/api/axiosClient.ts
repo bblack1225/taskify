4db37fcbe27f05311aa8605d8d8c64718b758ae1
bblack1225 <black60137@gmail.com>
2023-12-10 23:43:47 +0800

    feat: axiosClient 新增 request interceptor 負責塞入 token到header，但目前是寫死的狀態

--- taskify-frontend/src/api/axiosClient.ts
+++ taskify-frontend/src/api/axiosClient.ts
@@ -1,5 +1,15 @@
 import { notifications } from "@mantine/notifications";
-import axios from "axios";
+import axios, { InternalAxiosRequestConfig } from "axios";
+
+// const MOCK_TOKEN = 'eyJhbGciOiJIUzM4NCJ9.eyJzdWIiOiJibGFjazYwMTM3QGdtYWlsLmNvbSIsImV4cCI6MTcwMjc5MTAwNH0.J4KDh6obQ_wv1v9GvDJqru_kM8EoIfSIeUop_-zL5JCuvpsqo0f_KaUnn1nJ041H'
+function authRequestInterceptor(config: InternalAxiosRequestConfig) {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization= `Bearer ${token}`;
+  }
+  return config;
+}
+
 const axiosClient = axios.create({
   baseURL: "/api",
   headers: {
@@ -12,7 +22,9 @@ type ErrorType = {
   errorMessage: string;
 }
 
-//攔截器
+// setting up request interceptor
+axiosClient.interceptors.request.use(authRequestInterceptor);
+// setting up response interceptor
 axiosClient.interceptors.response.use(
   (response) => response.data,
   (error) => {
