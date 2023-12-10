1e9812fd1d48e3aed729709c52474b011c40ccf3
Keely <hellennnnn1@gmail.com>
2023-12-10 17:21:15 +0800

    feat: 新增getDayProps，沒選到時間時、當天日期會持續顯示粉紅色，新增點擊儲存按鈕時加入事件處理、setQueryData

--- taskify-frontend/src/components/Menu/TaskDateMenu.tsx
+++ taskify-frontend/src/components/Menu/TaskDateMenu.tsx
@@ -6,18 +6,73 @@ import {
   Text,
   Flex,
   Container,
+  Indicator,
 } from "@mantine/core";
-import { DatePicker } from "@mantine/dates";
+import { DatePicker, DatePickerProps } from "@mantine/dates";
 import { IconCalendarStats } from "@tabler/icons-react";
 import "@mantine/dates/styles.css";
 import { useState } from "react";
+import { useQueryClient } from "@tanstack/react-query";
+import { BaseDataRes, BaseTaskRes } from "@/types/column";
+import dayjs from "dayjs";
 
-function TaskDateMenu() {
+function TaskDateMenu({ task }: { task: BaseTaskRes }) {
   const [opened, setOpened] = useState(false);
   const [value, setValue] = useState<[Date | null, Date | null]>([null, null]);
 
-  console.log("value", value);
+  const queryClient = useQueryClient();
 
+  //now是要傳回給後端的
+  // const now = new Date().toISOString();
+
+  const handleDatePicker = () => {
+    if (value[0] === null || value[1] === null) {
+      return;
+    }
+    const start = value[0].toLocaleDateString();
+    const end = value[1].toLocaleDateString();
+    setOpened(false);
+    queryClient.setQueryData(["tasks"], (oldData: BaseDataRes) => {
+      return {
+        ...oldData,
+        tasks: oldData.tasks.map((oldTask) => {
+          if (oldTask.id !== task.id) {
+            return oldTask;
+          } else {
+            return {
+              ...oldTask,
+              startDate: start,
+              dueDate: end,
+            };
+          }
+        }),
+      };
+    });
+    return { start, end };
+  };
+
+  const getDayProps: DatePickerProps["getDayProps"] = (date) => {
+    if (
+      dayjs(value[0]).isSame(dayjs(date), "date") ||
+      dayjs(value[1]).isSame(dayjs(date), "date")
+    ) {
+      return {
+        style: {
+          backgroundColor: "var(--mantine-color-blue-filled)",
+          color: "var(--mantine-color-white)",
+        },
+      };
+    } else if (dayjs(date).isSame(dayjs(new Date()), "date")) {
+      return {
+        style: {
+          backgroundColor: "var(--mantine-color-pink-filled)",
+          color: "var(--mantine-color-white)",
+        },
+      };
+    }
+
+    return {};
+  };
   return (
     <Menu shadow="md" width={300} opened={opened} onChange={setOpened}>
       <Menu.Target>
@@ -37,19 +92,20 @@ function TaskDateMenu() {
           />
         </Menu.Label>
         <Text ta={"center"} size="xs" c={"blue"}>
-          請選擇『起始日期』及『截止日期』
+          {value[0] ? value[0].toLocaleDateString() : "未選擇"} - {""}
+          {value[1] ? value[1].toLocaleDateString() : "未選擇"}
         </Text>
         <Container mt={10} mb={10}>
           <Flex direction={"column"} align={"center"}>
             <DatePicker
-              defaultDate={new Date()}
+              getDayProps={getDayProps}
               type="range"
               allowSingleDateInRange
               value={value}
               onChange={setValue}
             />
           </Flex>
-          <Button mt={3} ml={7}>
+          <Button mt={3} ml={7} onClick={handleDatePicker}>
             儲存
           </Button>
         </Container>
