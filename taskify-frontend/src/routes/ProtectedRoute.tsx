32c942eddfc2737f859090b8ab9cabf78ab86445
bblack1225 <black60137@gmail.com>
2023-12-12 11:35:25 +0800

    fix: LabelProvider 先加回來

--- taskify-frontend/src/routes/ProtectedRoute.tsx
+++ taskify-frontend/src/routes/ProtectedRoute.tsx
@@ -1,6 +1,8 @@
 import MainLayout from "@/components/layout/MainLayout";
+import { LabelsProvider } from "@/context/LabelsProvider";
 import { Navigate, useLocation } from "react-router-dom";
 
+const BOARD_ID = "296a0423-d062-43d7-ad2c-b5be1012af96";
 export const ProtectedRoute = () => {
   const location = useLocation();
   const token = localStorage.getItem("token");
@@ -9,7 +11,9 @@ export const ProtectedRoute = () => {
   return (
     <>
       {isAuthenticated ? (
-        <MainLayout />
+        <LabelsProvider boardId={BOARD_ID}>
+          <MainLayout />
+        </LabelsProvider>
       ) : (
         <Navigate replace to={"/login"} state={{ from: location }} />
       )}
